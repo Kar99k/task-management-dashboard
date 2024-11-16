c625c876950180600cd28cde40ba4eef65455f6d
kar99k <krthkeyanlee@gmail.com>
2024-11-16 17:25:09 +0530

    - Responsive Grid

--- components/organism/TaskList.tsx
+++ components/organism/TaskList.tsx
@@ -5,7 +5,7 @@ import { mockTasks } from "@/data/mockData";
 const TaskList = () => {
   return (
     <div>
-      <div className="grid grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         {mockTasks.map((task, index) => {
           return (
             <TaskItemCard
